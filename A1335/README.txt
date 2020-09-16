7f9865a858ca9493396fc9358eed911f0618a514
Monti-17 <mithmonkeydoo-dar@hotmail.com>
2020-09-16 23:31:33 +1000

    Add files via upload

---
+++ A1335/README.txt
@@ -0,0 +1,7 @@
+# Allegro-A1335-Sensor-library
+A C++ library for communicating with the A 1335 hall effect encoder
+
+Arduino library for the Allegro A1335 Magnetic angle sensor. Designed for CJMCU-1335 breakout board
+The A1335 sensor detects the absolute angular position of a permanent magnet, typically a diametrically magnetized cylinder on the rotating shaft.
+
+Example program included to test sensor 
---
+++ A1335/examples/A1335_I2C_example/A1335_I2C_example.ino
@@ -0,0 +1,36 @@
+//Example code for the Allegro A1335 sensor based on the library built by ScranchNew 
+//https://github.com/ScranchNew/Allegro-A1335-Sensor-library/blob/master/A1335.h
+
+#include <A1335.h>
+#include <Wire.h> 
+
+A1335 angle;                                            //rename A1335 to 'angle' for ease of use
+
+
+void setup() {
+  Serial.begin(9600); 
+  A1335 start(int16_t);                                 //initiate sensor at 0x0C I2C address
+}
+
+void loop() {
+  float Direction = angle.readAngle();
+  int FieldRaw = angle.readFieldRaw();
+
+  if (FieldRaw <= 10)                                   //magnetic field is less than 10mT
+  {
+    Direction = 0;                                      //if the magnet is too far away (less than 10mT flux density), return default 0º value
+  }
+  Serial.print("Direction: ");
+  Serial.print(Direction);                              //prints angle in degrees
+  Serial.print("º");  
+  Serial.print("\t\tInternal Temp: ");
+  Serial.print(angle.readTemp() -273.15);               //prints internal temperature of chip in Celcius
+  Serial.print("ºC");  
+  Serial.print("\t\tField Strength: ");
+  Serial.print(angle.readField());                      //prints magnetic flux density in Tesla
+  Serial.print("T");    
+  Serial.print("\t\tRaw Field Strength: ");
+  Serial.print(angle.readFieldRaw());                   //prints magnetic flux density in milli Tesla
+  Serial.println("mT");  
+  delay(500);                                           //take reading every half a second
+}
