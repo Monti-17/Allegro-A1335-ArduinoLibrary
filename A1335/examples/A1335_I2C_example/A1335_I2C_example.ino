515dffd686bee239445140a80422e1876f65ea9b
Monti-17 <mithmonkeydoo-dar@hotmail.com>
2020-09-16 23:37:59 +1000

    Update A1335_I2C_example.ino

--- A1335/examples/A1335_I2C_example/A1335_I2C_example.ino
+++ A1335/examples/A1335_I2C_example/A1335_I2C_example.ino
@@ -1,14 +1,14 @@
-//Example code for the Allegro A1335 sensor based on the library built by ScranchNew 
-//https://github.com/ScranchNew/Allegro-A1335-Sensor-library/blob/master/A1335.h
+//Example code for the Allegro A1335 sensor based on the library built by ScranchNew
+//https://github.com/Monti-17/Allegro-A1335-ArduinoLibrary.git
 
 #include <A1335.h>
-#include <Wire.h> 
+#include <Wire.h>
 
 A1335 angle;                                            //rename A1335 to 'angle' for ease of use
 
 
 void setup() {
-  Serial.begin(9600); 
+  Serial.begin(9600);
   A1335 start(int16_t);                                 //initiate sensor at 0x0C I2C address
 }
 
@@ -22,15 +22,15 @@ void loop() {
   }
   Serial.print("Direction: ");
   Serial.print(Direction);                              //prints angle in degrees
-  Serial.print("º");  
+  Serial.print("º");
   Serial.print("\t\tInternal Temp: ");
   Serial.print(angle.readTemp() -273.15);               //prints internal temperature of chip in Celcius
-  Serial.print("ºC");  
+  Serial.print("ºC");
   Serial.print("\t\tField Strength: ");
   Serial.print(angle.readField());                      //prints magnetic flux density in Tesla
-  Serial.print("T");    
+  Serial.print("T");
   Serial.print("\t\tRaw Field Strength: ");
   Serial.print(angle.readFieldRaw());                   //prints magnetic flux density in milli Tesla
-  Serial.println("mT");  
+  Serial.println("mT");
   delay(500);                                           //take reading every half a second
 }
